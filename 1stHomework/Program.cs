a6369b45bdf4ae21ad0f5c0ecb6cf52bd170bacc
ADILET <alpha.z91@mail.ru>
2018-12-22 20:58:10 +0600

    Добавьте файлы проекта.

---
+++ 1stHomework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 1. Создать простое консольное приложение и ознакомиться с его структурой
+// 2. Написать приложение, которое выведет на экран Ваше имя и фамилию.
+// 3. Написать приложение, которое ожидает ввода нескольких чисел и выводит на экран их сумму.
+
+namespace FirstHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+
+            Console.WriteLine("My name is Adilet. Adilet Akhmetkali");
+
+            Console.ReadKey();
+        }
+    }
+}
